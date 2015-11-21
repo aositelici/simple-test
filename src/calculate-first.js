285ad4a102a6737c75709e77d50cf0fc20126ae3
liya <liya013@live.com>
2015-11-21 17:26:24 +0800

    finish first part,finish second part

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,3 +1,20 @@
 function single_element(arr) {
-  
+  var result = [];
+  var newArr = arr.filter(function(item, i) {
+    return i%2 !== 0;
+  });
+  //result.push(newArr[0]);
+  newArr.forEach(function(item) {
+    if (result.indexOf(item) === -1) {
+      result.push(item);
+    }else {
+      for(var i =0;i<result.length;i++) {
+        if(result[i] === item) {
+          result.splice(i,1);
+        }
+      }
+    }
+  });
+
+  return result;
 }
