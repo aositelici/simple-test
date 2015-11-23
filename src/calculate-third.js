b4c3ff3dba7b7e924b800eedd91be2bd595a0acb
liya <liya013@live.com>
2015-11-23 16:48:16 +0800

    refactor

--- src/calculate-third.js
+++ src/calculate-third.js
@@ -1,17 +1,36 @@
 function even_group_calculate_average(arr) {
-  var result = [];
-  var newArr = arr.filter(function(item, i) {
-    return i % 2 !== 0;
-  });
+  var evenArray = getEvenArray(arr);
+  var groups = getGroups(evenArray);
+  return calculateAverage(groups);
+}
 
-  var evenArray = newArr.filter(function(item, i) {
+function getEvenArray(array) {
+  var evenArray = array.filter(function(item, i) {
+    return i % 2 !== 0;
+  })
+  .filter(function(item, i) {
     return item % 2 === 0;
   });
 
   if(evenArray.length === 0) {
     evenArray.push(0);
   }
+  return evenArray;
+}
 
+function calculateAverage(groups) {
+  var result = [];
+  groups.forEach(function(group) {
+      var length = group.length;
+      var sum = group.reduce(function(a, b) {
+        return a + b;
+      });
+      result.push(sum/length);
+  });
+  return result;
+}
+
+function getGroups(evenArray) {
   var groups = [[], [], []];
   evenArray.forEach(function(item) {
     groups[item.toString().length - 1].push(item);
@@ -20,13 +39,5 @@ function even_group_calculate_average(arr) {
   var filterGroups = groups.filter(function(group) {
     return (group.length !== 0);
   });
-  filterGroups.forEach(function(group) {
-      var length = group.length;
-      group = group.reduce(function(a, b) {
-        return a + b;
-      });
-      result.push(group/length);
-  });
-
-  return result;
+  return filterGroups;
 }
