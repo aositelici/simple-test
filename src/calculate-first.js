930b354fe0d1a90ac198737adcde5e31c65067d6
liya <liya013@live.com>
2015-11-23 16:59:42 +0800

    format

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,15 +1,15 @@
 function single_element(arr) {
   var result = [];
   var newArr = arr.filter(function (item, i) {
-    return i%2 !== 0;
+    return i % 2 !== 0;
   });
 
   var repeatArray = [];
-  newArr.forEach(function (item){
+  newArr.forEach(function(item) {
     result.indexOf(item) === -1 ? result.push(item) : repeatArray.push(item);
   });
 
-  repeatArray.forEach(function (item){
+  repeatArray.forEach(function(item) {
     var index = result.indexOf(item);
     if(index !== -1) {
       result.splice(index,1);
--- src/calculate-second.js
+++ src/calculate-second.js
@@ -1,16 +1,15 @@
 function calculate_median(arr) {
   var result = 0;
   var newArr = arr.filter(function(item, i) {
-    return i%2 !== 0;
+    return i % 2 !== 0;
   });
 
-  if (newArr.length%2 ===1) {
+  if (newArr.length % 2 === 1) {
     newArr.sort();
-    result = newArr[(newArr.length-1)/2];
+    result = newArr[(newArr.length-1) / 2];
   } else {
     newArr.sort();
-    result = (newArr[(newArr.length+1)/2]+newArr[(newArr.length+1)/2-1])/2;
-    console.log(newArr[(newArr.length-1)/2]);
+    result = (newArr[(newArr.length) / 2] + newArr[(newArr.length) / 2 - 1]) / 2;
   }
   return result;
 }
