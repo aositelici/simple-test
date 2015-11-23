3e5b26152e87dbb6cc713d44be0c0768fcf0da74
liya <liya013@live.com>
2015-11-23 15:16:16 +0800

    fix the error that tesult include 11 for the second test

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,18 +1,18 @@
 function single_element(arr) {
   var result = [];
-  var newArr = arr.filter(function(item, i) {
+  var newArr = arr.filter(function (item, i) {
     return i%2 !== 0;
   });
-  //result.push(newArr[0]);
-  newArr.forEach(function(item) {
-    if (result.indexOf(item) === -1) {
-      result.push(item);
-    }else {
-      for(var i =0;i<result.length;i++) {
-        if(result[i] === item) {
-          result.splice(i,1);
-        }
-      }
+
+  var repeatArray = [];
+  newArr.forEach(function (item){
+    result.indexOf(item) === -1 ? result.push(item) : repeatArray.push(item);
+  });
+
+  repeatArray.forEach(function (item){
+    var index = result.indexOf(item);
+    if(index !== -1) {
+      result.splice(index,1);
     }
   });
 
