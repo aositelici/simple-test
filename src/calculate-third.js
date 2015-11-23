4a8a99e5f7c11980d057e7592a72768ef276620d
liya <liya013@live.com>
2015-11-23 16:24:47 +0800

    finish the third subjuect

--- src/calculate-third.js
+++ src/calculate-third.js
@@ -1,3 +1,32 @@
 function even_group_calculate_average(arr) {
-  
+  var result = [];
+  var newArr = arr.filter(function(item, i) {
+    return i % 2 !== 0;
+  });
+
+  var evenArray = newArr.filter(function(item, i) {
+    return item % 2 === 0;
+  });
+
+  if(evenArray.length === 0) {
+    evenArray.push(0);
+  }
+
+  var groups = [[], [], []];
+  evenArray.forEach(function(item) {
+    groups[item.toString().length - 1].push(item);
+  });
+
+  var filterGroups = groups.filter(function(group) {
+    return (group.length !== 0);
+  });
+  filterGroups.forEach(function(group) {
+      var length = group.length;
+      group = group.reduce(function(a, b) {
+        return a + b;
+      });
+      result.push(group/length);
+  });
+
+  return result;
 }
